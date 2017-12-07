5b9cc13347b3a88ae6fb8b2409f23cc93b8ab967
chomado (Madoka Chiyoda) <madoka.madoka@microsoft.com>
2017-12-06 16:36:26 -0800

    to control player

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,13 +4,21 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public float Speed;
+    private Rigidbody rb;
+
+    private void Start()
+    {
+        this.rb = GetComponent<Rigidbody>();
+    }
+    // called before performing any physics calculations (rigitbody toka)
+    private void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis(axisName: "Horizontal");
+        float moveVertical = Input.GetAxis(axisName: "Vertical");
+
+        var movement = new Vector3(x: moveHorizontal, y: 0.0f, z: moveVertical);
+
+        this.rb.AddForce(force: movement * this.Speed);
+    }
 }
