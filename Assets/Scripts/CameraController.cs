03d111fb536183642be9e6574ea8833d323875c9
chomado (Madoka Chiyoda) <madoka.madoka@microsoft.com>
2017-12-06 17:52:11 -0800

    new camera controller

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    public GameObject Player;
+    private Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+        this.offset = this.transform.position - this.Player.transform.position;
+	}
+	
+	void LastUpdate () {
+        this.transform.position = this.Player.transform.position + this.offset;
+	}
+}
